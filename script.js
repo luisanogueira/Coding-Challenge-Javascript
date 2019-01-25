3d3086d9a158774c33e6c5ebda58926a54a2c25d
Luisa Nogueira <luisa.nogueira@outlook.com>
2019-01-25 18:49:16 +0100

    final changes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const info = document.getElementById("dataHotel");
+const eachDay = document.getElementById("eachDay");
 const url = "https://switch-dev.seekda.com/properties/DEMO_CODE_01/bookability.json";
 
 function createNode(element) {
@@ -9,24 +10,41 @@ function append(parent, el) {
     return parent.appendChild(el);
 }
 
-fetch(url)
-.then((resp) => resp.json())
-.then(function(result){
-    let dataHotel = result.data;
-    let div = createNode('div');
-    let days = dataHotel.availabilities[0];
+function checkAvailability() {
+    info.innerHTML = "";
+    eachDay.innerHTML = "";
+    let calendar = document.getElementById("calendar").value;
+    let number = document.getElementById("number").value;
+    let language = document.getElementById("language").value;
+    
+    fetch(url + `?startDate=${calendar}&numberOfDays=${number}`)
+    .then(resp => resp.json())
+    .then(result => {
+        let dataHotel = result.data;
+        let description = createNode('div');
 
-    div.innerHTML = `<p>Languages: <br />${dataHotel.availableLanguages}</p>
-    <p>Time Zone: <br />${dataHotel.timeZone}</p>
-    <p>Currency: <br />${dataHotel.defaultCurrency}</p>`;
+       /* description.innerHTML = `<p>Languages: <br />${dataHotel.availableLanguages}</p>
+        <p>Time Zone: <br />${dataHotel.timeZone}</p>
+        <p>Currency: <br />${dataHotel.defaultCurrency}</p>`; */
 
-    for(let i=0; i < days.length; i++) {
+        let days = dataHotel.availabilities[0];
+        let nextDay = new Date(calendar);
 
-        if(days[i] == 0) {
-            document.getElementById("eachDay").innerHTML = `<td>Not available ${days[i]}</td>`;
-        } else {
-            document.getElementById("eachDay").innerHTML = `<td>Available ${days[i]}</td>`;
+        for(let i=0; i < days.length; i++) {
+            let day = createNode('div');
+
+            if(days[i] == 0) {
+                day.innerHTML = 
+                `<div class="card"><h5 class="card-title">${nextDay.toLocaleDateString(language)}</h5><p>Not available</p></div>`;
+            } else {
+                day.innerHTML = 
+                `<div class="card"><h5 class="card-title">${nextDay.toLocaleDateString(language)}</h5><p>Not available</p></div>`;
+            }
+            nextDay.setDate(nextDay.getDate() + 1);
+            append(eachDay, day);
         }
-    }
-    append(info, div);
-    })
+
+        append(info, description);
+        
+        })
+}
